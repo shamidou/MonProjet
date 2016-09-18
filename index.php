e0dc9276929e017570703f45d0a2edc31743da72
prof <prof@utilisateurWin>
2016-09-18 09:32:02 +0200

    commit camille

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       echo "Bonjour" ;
+       echo "Bonjour cest un autre commit" ;
         ?>
     </body>
 </html>
